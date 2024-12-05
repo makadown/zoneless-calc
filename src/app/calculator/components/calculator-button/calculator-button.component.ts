b1ffcac5747838ece8b9805bd9910c25f8e6e63f
Mario Arturo Serrano Flores <mario.serrano@imssbienestar.gob.mx>
2024-12-05 11:26:00 -0800

    agregando host listeners

--- src/app/calculator/components/calculator-button/calculator-button.component.ts
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -1,4 +1,8 @@
-import { ChangeDetectionStrategy, Component, HostBinding, input, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy, Component, ElementRef, HostBinding,
+  input, OnInit,
+  output, signal, viewChild, ViewEncapsulation
+} from '@angular/core';
 
 @Component({
   selector: 'calculator-button',
@@ -6,20 +10,27 @@ import { ChangeDetectionStrategy, Component, HostBinding, input, OnInit, ViewEnc
   imports: [],
   templateUrl: './calculator-button.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  host: { class: 'w-1/4 border-r border-b border-indigo-400' },
+  host: {
+    class: 'w-1/4 border-r border-b border-indigo-400',
+
+  },
   styleUrls: ['./calculator-button.component.css'],
   // encapsulation: ViewEncapsulation.None,
 })
 export class CalculatorButtonComponent implements OnInit {
+  public isPressed = signal(false);
+  public onClick = output<string>();
+  public contentValue = viewChild<ElementRef<HTMLButtonElement>>('button');
+
   public isCommand = input(false, {
-     transform: (value: boolean | string) =>
-       typeof value === 'string' ? value === '' : value
-    });
+    transform: (value: boolean | string) =>
+      typeof value === 'string' ? value === '' : value
+  });
 
   public isDoubleSize = input(false, {
     transform: (value: boolean | string) =>
       typeof value === 'string' ? value === '' : value
-   });
+  });
 
   // @HostBinding('class.is-command') get commandStyle() {
   //   return this.isCommand();
@@ -31,5 +42,22 @@ export class CalculatorButtonComponent implements OnInit {
 
   ngOnInit(): void {
 
-   }
+  }
+
+  handleClick() {
+    if (!this.contentValue()?.nativeElement) {
+      return;
+    }
+
+    const value = this.contentValue()!.nativeElement.innerText as string;
+    this.onClick.emit(value.trim().toUpperCase());
+  }
+
+  public keyboardPressedStyle(key: string) {
+    if (!this.contentValue()) return;
+    const value = this.contentValue()!.nativeElement.innerText as string;
+    if(value !== key) return;
+    this.isPressed.set(true);
+    setTimeout(() => this.isPressed.set(false), 100);
+  }
 }
--- src/app/calculator/components/calculator/calculator.component.ts
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -11,9 +11,24 @@ import { CalculatorButtonComponent } from '../calculator-button/calculator-butto
   ],
   templateUrl: './calculator.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '(document:keyup)': 'handleKeyboardEvent($event)'
+  }
   // styles: `
   //   .is-command {
   //     @apply bg-indigo-700 bg-opacity-20;
   //   }`
 })
-export class CalculatorComponent { }
+export class CalculatorComponent {
+
+  handleClick(key: string) {
+    console.log({ key: key.toLowerCase() });
+  }
+
+  // @HostListener('document:keyup', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    console.log(event, event.key);
+    this.handleClick(event.key);
+  }
+
+}
